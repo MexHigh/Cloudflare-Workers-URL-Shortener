aa37ca468e29139450d488dde448341ec83a8113
Leon Schmidt <mail@leon.wtf>
2023-02-08 20:29:38 +0100

    Popup now working :)

--- chrome-extension/popup.js
+++ chrome-extension/popup.js
@@ -1,11 +1,18 @@
+// get all required elements
+let unconfiguredBanner = document.querySelector("#unconfiguredBanner")
+let shortnameInput = document.querySelector("#shortname")
+let submitButton = document.querySelector("#submit")
+let toAdminPage = document.querySelector("#toAdminPage")
+let form = document.querySelector("form")
+let errMsg = document.querySelector("#errMsg")
+
+// get option values from local storage
+let storage = await chrome.storage.local.get(["server-host", "admin-token"])
+
 async function handleSubmit(event) {
     event.preventDefault()
-    
-    let shortname = document.querySelector("#shortname")?.value
-    
-    // get option values from local storage
-    // TODO, redirect to option page, if any of them is null
-    let storage = await chrome.storage.local.get(["server-host", "admin-token"])
+
+    event.submitter.setAttribute("aria-busy", "true")
 
     // get current URL
     let [tab] = await chrome.tabs.query({ active: true, lastFocusedWindow: true })
@@ -17,17 +24,37 @@ async function handleSubmit(event) {
             "x-api-key": storage["admin-token"]
         },
         body: JSON.stringify({
-            "name": shortname,
+            "name": shortnameInput.value,
             "target": currentURL
         })
-    }).then((r) => {
+    }).then(async (r) => {
+        event.submitter.removeAttribute("aria-busy")
         if (r.ok) {
-            console.log("ok")
+            errMsg.innerHTML = `Success: <a target="_blank" href="https://${storage["server-host"]}/${shortnameInput.value}">s.leon.wtf/${shortnameInput.value}</a>`
         } else {
-            console.log("not ok")
+            errMsg.innerHTML = await r.text()
         }
+    }).catch((e) => {
+        event.submitter.removeAttribute("aria-busy")
+        errMsg.innerHTML = e.message
     })
 }
 
-let form = document.querySelector("form")
-form?.addEventListener("submit", handleSubmit)
+// show banner if extension is unconfigured
+if (!storage["server-host"] || !storage["admin-token"]) {
+    unconfiguredBanner.hidden = false
+    shortnameInput.disabled = true
+    submitButton.disabled = true
+
+    // set click listener for option page link
+    document.querySelector("#toOptionsPage")?.addEventListener("click", (event) => {
+        event.preventDefault()
+        chrome.runtime.openOptionsPage()
+    })
+} else {
+    // set admin page link
+    toAdminPage.href = `https://${storage["server-host"]}/admin`
+
+    // add submit listener for adding shortlinks
+    form?.addEventListener("submit", handleSubmit)
+}
